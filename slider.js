902988b37fac4917b72676666ce291c67931a74c
Zeljko012 <98706961+Zeljko012@users.noreply.github.com>
2022-07-21 17:01:54 +0200

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,54 +1,31 @@
-( function($) {
-  
-    $(document).ready(function() {
-      
-      var s           = $('.slider'),
-          sWrapper    = s.find('.slider-wrapper'),
-          sItem       = s.find('.slide'),
-          btn         = s.find('.slider-link'),
-          sWidth      = sItem.width(),
-          sCount      = sItem.length,
-          slide_date  = s.find('.slide-date'),
-          slide_title = s.find('.slide-title'),
-          slide_text  = s.find('.slide-text'),
-          slide_more  = s.find('.slide-more'),
-          slide_image = s.find('.slide-image img'),
-          sTotalWidth = sCount * sWidth;
-      
-      sWrapper.css('width', sTotalWidth);
-      sWrapper.css('width', sTotalWidth);
-      
-      var clickCount  = 0;
-      
-      btn.on('click', function(e) {
-        e.preventDefault();
-  
-        if( $(this).hasClass('next') ) {
-          
-          ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
-        } else if ( $(this).hasClass('prev') ) {
-          
-          ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
-        }
-        TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
-  
-  
-        //CONTENT ANIMATIONS
-  
-        var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
-        var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
-  
-        TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
-        TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
-        TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
-        TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
-        TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
-  
-      });
-            
-    });
-  })(jQuery);
-  
-  $('.overlay').addClass('overlay-blue');
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
 
-  
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
