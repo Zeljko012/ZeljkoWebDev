4713233c4eb5d356dd8a8aef81296418d7d8c933
Željko Prović <98706961+Zeljko012@users.noreply.github.com>
2023-05-09 16:33:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function redirectToPage() {
 
 
 
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
