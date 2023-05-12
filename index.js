2b4636ef7b14417f51da9866cf582b9151a533d4
Željko Prović <98706961+Zeljko012@users.noreply.github.com>
2023-05-12 22:30:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -16,5 +16,27 @@ function redirectToPage() {
   window.location.href = "https://www.example.com"; // zamijenite sa stvarnom URL adresom ciljne stranice
 }
 
+// Email form
+
+document.getElementById('my-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // sprečava podnošenje forme
+
+  // Dohvatanje vrednosti iz forme
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Slanje e-poruke putem Email.js
+  emailjs.send('service_fo9a1nr', 'template_btfj4vo', {
+    to_email: 'proviczeljko@gmail.com',
+    from_name: name,
+    from_email: email,
+    message: message
+  }).then(function() {
+    alert('Poruka je uspešno poslata!');
+  }).catch(function(error) {
+    console.error('Došlo je do greške prilikom slanja poruke:', error);
+  });
+});
 
 
